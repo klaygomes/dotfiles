e400827293612671a98c6ff590fdc8a22084956e
Cleiton Loiola <klaygomes@gmail.com>
2021-08-13 23:17:57 +0200

    Include fzf, fd and bat

--- zsh/variables.sh
+++ zsh/variables.sh
@@ -6,3 +6,7 @@ export PATH=$ANDROID_HOME/platform-tools:$PATH
 
 # Compilation flags
 export ARCHFLAGS=$(uname -m)
+
+# fzf and fd integration
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+
