6b60646b8b9e007add0145fd0a78af20c0d3330b
Cleiton Loiola <klaygomes@gmail.com>
2023-01-27 21:36:02 +0100

    Include default locales

--- zsh/variables.sh
+++ zsh/variables.sh
@@ -3,3 +3,6 @@ if type rg &> /dev/null; then
   export FZF_DEFAULT_COMMAND='rg --files'
   export FZF_DEFAULT_OPTS='-m --height 50% --border'
 fi
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
