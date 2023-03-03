1abf635aa485d6b4dd713f99360d65f38ef3a9c1
Cleiton Loiola <klaygomes@gmail.com>
2023-03-03 22:30:56 +0100

    Plugins not used

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -3,10 +3,7 @@ antigen bundle git
 antigen bundle command-not-found
 antigen bundle "chitoku-k/fzf-zsh-completions"
 antigen bundle zsh-users/zsh-autosuggestions
-
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
-# Prompt style.
-antigen bundle nojhan/liquidprompt
 # Tell Antigen that you're done.
 antigen apply
