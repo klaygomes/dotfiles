c57dd515b17ed474234dfde143550dcfed054ad3
Cleiton Loiola <klaygomes@gmail.com>
2026-02-11 16:22:04 +0100

    Improve ps1

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -euo pipefail
 
+# only owner can delete its files
+umask 022
+
 # avoid overwriting existing files with output redirection
 set -o noclobber
+
+# Enable parameter expansion in the prompt
+setopt PROMPT_SUBST
+
+# Load Git info
+autoload -Uz vcs_info
+precmd() {
+    vcs_info
+    IP_DATA=$(get_ip_info)
+}
+zstyle ':vcs_info:git:*' formats ' ~ git (%b) '
+
+PROMPT='%F{magenta}%n@${IP_DATA}${vcs_info_msg_0_}'$'\n''%~%f %(!.#.$) '
