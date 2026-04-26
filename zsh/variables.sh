c8fe2ee03f330706d4b159e9f8a05db5b2fb8118
Cleiton Loiola <klaygomes@gmail.com>
2026-04-26 23:56:17 +0200

    Refactor meeting-search SKILL and set default EDITOR
    - Compact SKILL.md: shorter output format, bootstrap via shell script, remove Agent tool
    - Add EDITOR=tmux-vim to zsh variables

--- zsh/variables.sh
+++ zsh/variables.sh
@@ -11,3 +11,4 @@ export LC_ALL=en_US.UTF-8
 export MAKEFLAGS="-j $(nproc 2>/dev/null || sysctl -n hw.logicalcpu)" # Use maximum number of cores for make
 export CONFIG_PATH=$HOME/.config/
 export PATH="/Users/cleiton.loiola/bin:$PATH"
+export EDITOR="tmux-vim"
